bc6b5b9d0bb9e25733c8811bd81c5460e99895b5
lalithesh11 <lalithesh.11@gmail.com>
2021-01-30 08:38:36 +0530

    modified routes

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,9 @@ const App=()=>{
 <Switch>
 
 {/* <Route exact path="/" render={()=><About name={'Lalithesh'}/>}/> */}
-<Route exact path="/" render={()=><Search />}/>
+{/* <Route exact path="/" render={()=><Search />}/> */}
+{/* For github, use the below Route */}
+<Route exact path="/LiveSearchFilter/" render={()=><Search />}/>
 {/* <Route exact  path="/contact" component={Contact}/> */}
    {/* <Route path="/services" render={()=><Services val={'will do all kind of services'}/>}/> */}
    {/* <Route path="/contact/name" component={Name}/> */}
@@ -33,7 +35,9 @@ const App=()=>{
 
    {/* In case , if we have multiple parameters in url, we can add extra variable.But, we should pass two parameters without missing. In case if we pass only one parameter, it will route to error component*/}
    {/* <Route path="/user/:fname/:lname" component={User}/> */}
-   <Route path="/search" component={Search}/>
+   {/* <Route path="/search" component={Search}/> */}
+   {/* For github, use the below Route */}
+   <Route path="/LiveSearchFilter/search" component={Search}/>
    <Route component={()=><Error/>}/>
 
 </Switch>
